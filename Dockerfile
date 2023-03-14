97d33c310c906c8ab23a22be2cd23b124c184afa
Abhisht <abhishtchouhan@gmail.com>
2023-03-14 06:30:05 +0530

    Added Docker

--- Dockerfile
+++ Dockerfile
@@ -7,4 +7,6 @@ RUN pip3 install -r requirements.txt
 
 COPY . .
 
-CMD [ "python3", "-m" , "flask", "run", "--host=0.0.0.0"]
+ENTRYPOINT [ "python3" ]
+
+CMD ["-m","main.py"]
--- main.py
+++ main.py
@@ -2,44 +2,28 @@ import flask
 import os
 import json
 from db import dbname
+from flask_cors import CORS, cross_origin
+
 db = dbname()
 
 a=flask.Flask(__name__)
-
-@a.route('/executequery',methods=['GET','POST'])
-def LoadData():
-    s=flask.request.get_json()
-    try:
-        print("[System] Executing query...")
-        data=os.popen(s['query']).read()
-        return(flask.jsonify(json.loads(data)))
-    except Exception:
-        data={"ERROR":"no data available."}
-        return(flask.jsonify(data))
+cors = CORS(a)
+a.config['CORS_HEADERS'] = 'Content-Type'
 
 
 
 
 
 @a.route('/aws/benchmark/executequery',methods=['GET','POST'])
+@cross_origin()
 def AwsCompliance():
     s=flask.request.get_json()
     print(s)
-    # collection_name = db.aws
     try:
-        print("[System] Executing query...")
+        print("[System] Executing Query...")
         data=os.popen(s['query']).read()
+        print("[System] Executed Query...")
         return(flask.jsonify(json.loads(data)))
-        # if s['op'] == "audit_manager":
-        #     print("[System] Executing query...")
-        #     data=os.popen(s['query']).read()
-            # item = {
-            #     "id": s['id'],
-            #     "data": json.loads(data)
-            # }
-            # collection_name.insert_one(item)
-
-            # return(flask.jsonify(json.loads(data)))
 
     except Exception(err):
         data={"ERROR":"no data available."}
@@ -47,4 +31,4 @@ def AwsCompliance():
 
 
 if __name__=='__main__':
-    a.run(debug=True, port=5002)
+    a.run(debug=True, host='0.0.0.0', port=5002)
